1b6dc98fde716d3771d2eabacd62d38157de86a0
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-08-14 14:51:10 +0530

    folder structure created

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
-app.use(express.json());
-connectDB();
-app.use("/api/v1", require("./routes/questions"));
-// app.use("/api/v2", require("./routes/user"));
-// app.use("/api/v3", require("./routes/responses"));
-// app.use("/api/v4", require("./routes/feedback"));
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
-});
+require("dotenv").config();
+const express=require("express");
+const bodyparser=require("body-parser");
+// const postRouter=require("./routers/postTask.js");
+// const getRouter=require("./routers/getTask.js");
+const PORT=6000;
+const app=express(); 
+const cors=require("cors");
+require("./config/db.js");  
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(cors())
+
+
+// app.use("/api",postRouter);
+// app.use("/api",getRouter);   
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        msg:"Hello, Welcome To This Page"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is live")
+})
